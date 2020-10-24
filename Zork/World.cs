54957f99603ceb4928bc2300dad0e43245367b98
Chayes315 <Chayes315@users.noreply.github.com>
2020-10-24 00:03:21 -0400

    4.3
    4.3?

---
+++ Zork/World.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Zork
+{
+    public enum Directions
+    {
+        North,
+        South,
+        East,
+        West
+    }
+    public class World
+    {
+        public HashSet<Room> Rooms { get; set; }
+
+        [JsonIgnore]
+        public IReadOnlyDictionary<Directions, Room> Neighbors { get; private set;}
+
+        public Player SpawnPlayer() => new Player(this, StartingLocation);
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            mRoomsByName = Rooms.ToDictionary(room => room.Name, room => room);
+
+            foreach (Room room in Rooms)
+            {
+                room.UpdateNeighbors(this);
+            }
+        }
+
+        [JsonProperty]
+        private string StartingLocation { get; set; }
+
+        private Dictionary<string, Room> mRoomsByName;
+    }
+}
